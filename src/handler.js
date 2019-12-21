1a3e98d8bbcadeadcd7a92dc118a8493506ab557
Tusko <tusko@photoinside.me>
2019-12-21 09:41:50 +0200

    fx json

--- src/handler.js
+++ src/handler.js
@@ -3,12 +3,16 @@ const mcache = require("memory-cache");
 const log = (...args) => console.log("→", ...args);
 
 const validURL = str => {
-  regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  if (regexp.test(str)) {
-    return true;
-  } else {
-    return false;
-  }
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // ip (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + //port
+    "(\\?[;&amp;a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return pattern.test(str);
 };
 
 exports.response = (err = false, msg = "ok", shortURL) => {
@@ -50,7 +54,7 @@ exports.processing = (req, res) => {
 
   axios(parsedUrl)
     .then(res => {
-      mcache.put(cacheKey, json, cacheTime * 1000);
+      mcache.put(cacheKey, res.data, cacheTime * 1000);
       res.json(this.response(true, res.data));
     })
     .catch(e => res.json(this.response(true, e.message)));
